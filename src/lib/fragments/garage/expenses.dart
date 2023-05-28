58c884aade6fef32d836e63a0e9b7bcf19164ee7
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2023-05-28 17:18:06 +0300

    Some changes to the listview

--- src/lib/fragments/garage/expenses.dart
+++ src/lib/fragments/garage/expenses.dart
@@ -7,20 +7,25 @@ class Expenses extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: 15,
-      itemBuilder: (BuildContext context, int index) {
-        return SquareWidget2(
-          icon: 'assets/icons/gas.svg',
-          upLeftText: "74,990 km",
-          upRightText: "Apr 29, 2023",
-          downLeftText: "Change Oil",
-          progress: 0.95,
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) {
-        return const SizedBox(height: 16);
-      },
+    // Add padding to the listview
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+          horizontal: 40, vertical: 8), // Adjust the padding value as needed
+      child: ListView.separated(
+        itemCount: 15,
+        itemBuilder: (BuildContext context, int index) {
+          return SquareWidget2(
+            icon: 'assets/icons/gas.svg',
+            upLeftText: "74,990 km",
+            date: DateTime.now(),
+            downLeftText: "Change Oil",
+            progress: 0.95,
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return const SizedBox(height: 50);
+        },
+      ),
     );
   }
 }
