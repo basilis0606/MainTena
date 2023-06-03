f649610dff17695045b493da840c8fc4e18d7424
Gpanaritis <jpanaritis2001@gmail.com>
2023-06-03 16:19:30 +0300

    Fixed Location

--- src/lib/classes/Location.dart
+++ src/lib/classes/Location.dart
@@ -1,15 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:try_project/classes/Expense.dart';
-import 'package:try_project/classes/ServiceManual.dart';
-
 class Location {
+  double latitude;
+  double longtitude;
 
-double latitude;
-double longtitude;
-
-}
-
-Location()
-{
-    
+  Location(this.latitude, this.longtitude);
 }
