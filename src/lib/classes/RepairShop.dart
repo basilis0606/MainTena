4b93a3ad60b3d4ce33416b2b1ce1a4b9a90cb564
Gpanaritis <jpanaritis2001@gmail.com>
2023-06-03 17:09:49 +0300

    Renamed and added constructor

---
+++ src/lib/classes/RepairShop.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import 'Location.dart';
+import 'Review.dart';
+
+class RepairShop {
+  String name;
+  String email;
+  String phone;
+  String website;
+  Location location;
+  List<int> services = [];
+  double member_discount;
+  Map? opening_hours;
+  Map? closing_hours;
+  List<Review> reviews = [];
+
+  RepairShop(
+      this.name,
+      this.email,
+      this.phone,
+      this.website,
+      this.location,
+      this.services,
+      this.member_discount,
+      List<TimeOfDay> openingHours,
+      List<TimeOfDay> closingHours) {
+    assert(openingHours.length == 7);
+    assert(closingHours.length == 7);
+
+    this.opening_hours = {
+      "Monday": openingHours[0],
+      "Tuesday": openingHours[1],
+      "Wednesday": openingHours[2],
+      "Thursday": openingHours[3],
+      "Friday": openingHours[4],
+      "Saturday": openingHours[5],
+      "Sunday": openingHours[6],
+    };
+
+    this.closing_hours = {
+      "Monday": closingHours[0],
+      "Tuesday": closingHours[1],
+      "Wednesday": closingHours[2],
+      "Thursday": closingHours[3],
+      "Friday": closingHours[4],
+      "Saturday": closingHours[5],
+      "Sunday": closingHours[6],
+    };
+  }
+
+  hasRepair(int repair) {
+    bool a;
+    for (final int b in services) {
+      if (repair == b) {
+        a = b;
+      }
+    }
+  }
+
+  addReview() {
+    reviews.add(Review);
+  }
+
+  showAllReviews() {
+    for (final Review in reviews) {
+      print(Review);
+    }
+  }
+}
