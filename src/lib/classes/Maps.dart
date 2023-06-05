b63ea91cdc3e573a73f03a77b53fc792f34c6869
Nikolas Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2023-06-05 16:48:47 +0300

    set new location on MAPS

---
+++ src/lib/classes/Maps.dart
@@ -0,0 +1,38 @@
+import 'Location.dart';
+import 'RepairShop.dart';
+
+class Map {
+
+Location user_location;
+
+  List<RepairShop>? repairShops;
+
+  Map([List<RepairShop>? repairShops]) {
+    this.repairShops = repairShops ?? [];
+  }
+
+  List<RepairShop> getClosestRepairShops(Location location, int quantity,
+      [int? serviceId]) {
+    if (serviceId != null) {
+      // Filter repair shops that can provide the specified service
+      List<RepairShop> filteredRepairShops = repairShops!
+          .where((repairShop) => repairShop.hasRepair(serviceId))
+          .toList();
+
+      // Return the first `quantity` repair shops that provide the service
+      return filteredRepairShops.sublist(0, quantity);
+    } else {
+      // Return the first `quantity` repair shops
+      return repairShops!.sublist(0, quantity);
+    }
+  }
+
+  getRepairShops() {
+    return repairShops;
+  }
+  getnewposition()
+  {
+    
+    this.user_location= Location();
+  }
+}
