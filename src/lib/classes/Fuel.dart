e771703f6217121450df11e9c1dd8cb34460b41d
Nikolas Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2023-06-05 17:09:54 +0300

    fixed errors in Fuel

--- src/lib/classes/Fuel.dart
+++ src/lib/classes/Fuel.dart
@@ -16,31 +16,4 @@ class Fuel extends notif.Notification {
             date: date,
             estimatedCost: estimatedCost,
             description: description);
-
-    void setLiters()
-    {
-         // Read input from the user
-  String input = stdin.readLineSync();
-
-  // Parse the input string to a double
-  double value = double.parse(input);
-
-      liters= value;    }
-
-    int checkDate() {
-    int cost
-    if(date> DateTime.now)
-    {
-        cost=this.estimatedCost
-    }
-
-    double egetLiters()
-    {
-
-        return this.liters
-    }
-
-    return cost;
-    throw UnimplementedError();
-  }
 }
