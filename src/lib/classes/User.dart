5ec52a24cf20c9a55a436306c6d37fa262937b97
Gpanaritis <basilis0606@gmail.com>
2023-06-05 17:15:03 +0300

    Added with constructor

--- src/lib/classes/User.dart
+++ src/lib/classes/User.dart
@@ -8,8 +8,10 @@ class User {
   String email;
   String phoneNumber;
   bool isMember;
-  Garage garage;
+  Garage garage = Garage();
   Newsfeed newsfeed = Newsfeed();
   Forum forum = Forum();
   Activity activity = Activity();
+
+  User(this.username, this.email, this.phoneNumber, this.isMember);
 }
