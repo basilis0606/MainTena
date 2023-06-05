3f880e701912b1a53e049c0207cbd1033aa22eeb
Gpanaritis <basilis0606@gmail.com>
2023-06-05 17:22:27 +0300

    Update User.dart

--- src/lib/classes/User.dart
+++ src/lib/classes/User.dart
@@ -14,4 +14,21 @@ class User {
   Activity activity = Activity();
 
   User(this.username, this.email, this.phoneNumber, this.isMember);
+
+  void addNewVehicle(String name, String plateNumber, String model,
+      String brand, double miles) {
+    garage.addNewVeh(name, plateNumber, model, brand, miles);
+  }
+
+  void addNewPost(String title, String content) {
+    activity.addPost(title, content);
+  }
+
+  void addNewComment(String comment) {
+    activity.addComment(comment);
+  }
+
+  void addNewReview(double rating, String text) {
+    activity.addReview(rating, text);
+  }
 }
