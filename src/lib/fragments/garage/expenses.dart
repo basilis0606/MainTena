99303aba7ee681ac57bd95f22d94e468b53f8256
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2023-06-06 13:33:13 +0300

    Added implementation with widget

--- src/lib/fragments/garage/expenses.dart
+++ src/lib/fragments/garage/expenses.dart
@@ -3,7 +3,7 @@ import 'package:try_project/classes/Maintenance.dart';
 import 'package:try_project/classes/Vehicle.dart';
 import 'package:try_project/classes/Fuel.dart';
 import 'package:try_project/widgets/square_widget.dart';
-import 'package:try_project/widgets/square_widget.dart';
+import '../../widgets/expenses_list.dart';
 
 class Expenses extends StatelessWidget {
   final Vehicle my_veh;
@@ -16,33 +16,7 @@ class Expenses extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(
           horizontal: 40, vertical: 8), // Adjust the padding value as needed
-      child: ListView.separated(
-        itemCount: my_veh.expenses_2.length,
-        itemBuilder: (BuildContext context, int index) {
-          if (my_veh.expenses_2[index].notification is Maintenance) {
-            return SquareWidget2(
-              icon: 'assets/icons/maintenance.svg',
-              upLeftText:
-                  "${my_veh.expenses_2[index].notification.mileage} km,",
-              date: my_veh.expenses_2[index].date,
-              downLeftText: "hello",
-              progress: 0.95,
-            );
-          } else if (my_veh.expenses_2[index].notification is Fuel) {
-            return SquareWidget2(
-              icon: 'assets/icons/gas.svg',
-              upLeftText: "${my_veh.expenses_2[index].notification.mileage} km",
-              date: my_veh.expenses_2[index].date,
-              downLeftText: (my_veh.expenses_2[index].notification as Fuel)
-                  .liters
-                  .toString(),
-            );
-          }
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return const SizedBox(height: 50);
-        },
-      ),
+      child: SquareList(my_veh: my_veh),
     );
   }
 }
