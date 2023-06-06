6dd855bb63bfce2c5144d6dde416fca5546f6fab
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2023-06-06 13:32:56 +0300

    Removed padding from widget

--- src/lib/widgets/expenses_list.dart
+++ src/lib/widgets/expenses_list.dart
@@ -19,60 +19,55 @@ class SquareList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Add padding to the listview
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-          horizontal: 40, vertical: 8), // Adjust the padding value as needed
-      child: ListView.separated(
-        itemCount: my_veh.expenses_2.length,
-        itemBuilder: (BuildContext context, int index) {
-          if (my_veh.expenses_2[index].notification is Maintenance) {
-            return SquareWidget2(
-                icon: 'assets/icons/maintenance.svg',
-                upLeftText:
-                    "${my_veh.expenses_2[index].notification.mileage} km,",
-                date: my_veh.expenses_2[index].date,
-                downLeftText:
-                    (my_veh.expenses_2[index].notification as Maintenance)
-                        .type);
-          } else if (my_veh.expenses_2[index].notification is Fuel) {
-            return SquareWidget2(
-              icon: 'assets/icons/gas.svg',
-              upLeftText: "${my_veh.expenses_2[index].notification.mileage} km",
-              date: my_veh.expenses_2[index].date,
-              downLeftText: (my_veh.expenses_2[index].notification as Fuel)
-                  .liters
-                  .toString(),
-            );
-          } else if (my_veh.expenses_2[index].notification is Damage) {
-            return SquareWidget2(
-              icon: 'assets/icons/damage.svg',
-              upLeftText: "${my_veh.expenses_2[index].notification.mileage} km",
-              date: my_veh.expenses_2[index].date,
-              downLeftText:
-                  (my_veh.expenses_2[index].notification as Damage).type,
-            );
-          } else if (my_veh.expenses_2[index].notification is Upgrade) {
-            return SquareWidget2(
-              icon: 'assets/icons/upgrade.svg',
-              upLeftText: "${my_veh.expenses_2[index].notification.mileage} km",
-              date: my_veh.expenses_2[index].date,
-              downLeftText:
-                  (my_veh.expenses_2[index].notification as Upgrade).type,
-            );
-          } else if (my_veh.expenses_2[index].notification is Insurance) {
-            return SquareWidget2(
-              icon: 'assets/icons/insurance.svg',
-              upLeftText: "${my_veh.expenses_2[index].notification.mileage} km",
-              date: my_veh.expenses_2[index].date,
-              downLeftText:
-                  (my_veh.expenses_2[index].notification as Insurance).type,
-            );
-          }
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return const SizedBox(height: 50);
-        },
-      ),
+    return ListView.separated(
+      itemCount: my_veh.expenses_2.length,
+      itemBuilder: (BuildContext context, int index) {
+        if (my_veh.expenses_2[index].notification is Maintenance) {
+          return SquareWidget2(
+            icon: 'assets/icons/maintenance.svg',
+            upLeftText: "${my_veh.expenses_2[index].notification.mileage} km,",
+            date: my_veh.expenses_2[index].date,
+            downLeftText:
+                (my_veh.expenses_2[index].notification as Maintenance).type,
+          );
+        } else if (my_veh.expenses_2[index].notification is Fuel) {
+          return SquareWidget2(
+            icon: 'assets/icons/gas.svg',
+            upLeftText: "${my_veh.expenses_2[index].notification.mileage} km",
+            date: my_veh.expenses_2[index].date,
+            downLeftText: (my_veh.expenses_2[index].notification as Fuel)
+                .liters
+                .toString(),
+          );
+        } else if (my_veh.expenses_2[index].notification is Damage) {
+          return SquareWidget2(
+            icon: 'assets/icons/damage.svg',
+            upLeftText: "${my_veh.expenses_2[index].notification.mileage} km",
+            date: my_veh.expenses_2[index].date,
+            downLeftText:
+                (my_veh.expenses_2[index].notification as Damage).type,
+          );
+        } else if (my_veh.expenses_2[index].notification is Upgrade) {
+          return SquareWidget2(
+            icon: 'assets/icons/upgrade.svg',
+            upLeftText: "${my_veh.expenses_2[index].notification.mileage} km",
+            date: my_veh.expenses_2[index].date,
+            downLeftText:
+                (my_veh.expenses_2[index].notification as Upgrade).type,
+          );
+        } else if (my_veh.expenses_2[index].notification is Insurance) {
+          return SquareWidget2(
+            icon: 'assets/icons/insurance.svg',
+            upLeftText: "${my_veh.expenses_2[index].notification.mileage} km",
+            date: my_veh.expenses_2[index].date,
+            downLeftText:
+                (my_veh.expenses_2[index].notification as Insurance).type,
+          );
+        }
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return const SizedBox(height: 50);
+      },
     );
   }
 }
